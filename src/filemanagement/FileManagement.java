081dbde18307e509e93fe6a887cbf14952c5d450
vali <vali0703@gmail.com>
2014-03-11 17:50:28 +0200

    Signed-off-by: vali <vali0703@gmail.com>

--- src/filemanagement/FileManagement.java
+++ src/filemanagement/FileManagement.java
@@ -4,20 +4,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
+
 
 public class FileManagement {
 
-	final String filepath = "4001.csv";
+	//final String filepath;
 
 	public void readingFile() throws IOException {
 		String[] field;
 		String[] conditionField;
 		String[] takingMinutes;
+		String[] name;
 		int value;
+		
+		Scanner filePath = new Scanner(System.in);
+		System.out.print("Please enter the File Path: ");
+		String filepath = filePath.nextLine();
+		
+		System.out.println(filepath);
+		name =  filepath.split("\\.");
+		System.out.println(name[1]);
+		
+		Scanner inputParser = new Scanner(System.in);
+		System.out.print("Enter the Time Divisor: ");
+		int timeDivizor = inputParser.nextInt();
 		
 		BufferedReader buffReader = new BufferedReader(new FileReader(filepath));
 		String firstLine = buffReader.readLine();
-		PrintWriter writer = new PrintWriter("4001managed.csv","UTF-8");
+		PrintWriter writer = new PrintWriter(name[0] + "managedBy" + timeDivizor + ".csv", "UTF-8");
 		writer.println(firstLine);
 		
 			while (true) {
@@ -32,11 +47,11 @@ public class FileManagement {
 				
 				value = new Integer(takingMinutes[1]);
 				
-				if(value % 3 == 0){
+				if(value % timeDivizor == 0){
 					writer.println(currentLine);
 				}
 			}
-		
+			
 		buffReader.close();
 		writer.close();
 	}
