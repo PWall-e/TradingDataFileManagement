95a6ae48276adde05be762f3efb96e11dd5e8a6f
vali <vali0703@gmail.com>
2014-03-12 21:48:57 +0200

    Signed-off-by: vali <vali0703@gmail.com>

--- src/filemanagement/FileManagement.java
+++ src/filemanagement/FileManagement.java
@@ -48,7 +48,7 @@ public class FileManagement {
 	}
 	
 	public static void main(String[] args) {
-		try {//the static method
+		try {
 			readingFile();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
