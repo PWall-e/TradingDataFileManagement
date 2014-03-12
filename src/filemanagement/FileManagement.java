0b4634fa5dc12e1c5224e91b7bee41cbf46c4006
vali <vali0703@gmail.com>
2014-03-12 22:05:55 +0200

    Signed-off-by: vali <vali0703@gmail.com>

--- src/filemanagement/FileManagement.java
+++ src/filemanagement/FileManagement.java
@@ -7,49 +7,45 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileManagement {
-
-	public static void readingFile() throws IOException {
-		Scanner filePath = new Scanner(System.in);
-		System.out.print("Please enter the File Path: ");
-		String filepath = filePath.nextLine();
-	
-		String[] fileName =  filepath.split("\\.");
-		
-		Scanner inputParser = new Scanner(System.in);
-		System.out.print("Enter the Minute Divisor: ");
-		int timeDivizor = inputParser.nextInt();
-		
-		BufferedReader buffReader = new BufferedReader(new FileReader(filepath));
-		String firstLine = buffReader.readLine();
-		PrintWriter writer = new PrintWriter(fileName[0] + "managedBy" + timeDivizor + ".csv", "UTF-8");
-		writer.println(firstLine);
-		
-			while (true) {
-				String currentLine = buffReader.readLine();
-				if (currentLine == null)
-					break;
-				String[] splitedLine = currentLine.split(",");
-				
-				String[] timeField = splitedLine[0].split(" ");
-				
-				String[] splitedTime = timeField[1].split(":");
-				
-				int minutesValue = new Integer(splitedTime[1]);
-				
-				if(minutesValue % timeDivizor == 0){
-					writer.println(currentLine);
-				}
-			}
-			
-		filePath.close(); 
-		inputParser.close();//Close Scanners
-		buffReader.close(); //Close reader
-		writer.close(); //Close writer
-	}
 	
 	public static void main(String[] args) {
 		try {
-			readingFile();
+			Scanner filePath = new Scanner(System.in);
+			System.out.print("Please enter the File Path: ");
+			String filepath = filePath.nextLine();
+		
+			String[] fileName =  filepath.split("\\.");
+			
+			Scanner inputParser = new Scanner(System.in);
+			System.out.print("Enter the Minute Divisor: ");
+			int timeDivizor = inputParser.nextInt();
+			
+			BufferedReader buffReader = new BufferedReader(new FileReader(filepath));
+			String firstLine = buffReader.readLine();
+			PrintWriter writer = new PrintWriter(fileName[0] + "managedBy" + timeDivizor + ".csv", "UTF-8");
+			writer.println(firstLine);
+			
+				while (true) {
+					String currentLine = buffReader.readLine();
+					if (currentLine == null)
+						break;
+					String[] splitedLine = currentLine.split(",");
+					
+					String[] timeField = splitedLine[0].split(" ");
+					
+					String[] splitedTime = timeField[1].split(":");
+					
+					int minutesValue = new Integer(splitedTime[1]);
+					
+					if(minutesValue % timeDivizor == 0){
+						writer.println(currentLine);
+					}
+				}
+				
+			filePath.close(); 
+			inputParser.close();//Close Scanners
+			buffReader.close(); //Close reader
+			writer.close(); //Close writer
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
